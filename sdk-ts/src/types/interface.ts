bcac73b808eb6fa3e0d0314e6c5daa123201de7b
Lucas Kenji Uezu <kenjiuezu3@gmail.com>
2025-02-20 14:21:54 -0300

    Update docstrings on sdk-ts

--- sdk-ts/src/types/interface.ts
+++ sdk-ts/src/types/interface.ts
@@ -2,6 +2,15 @@ import { NeonInvoker, NeonParser } from '@cityofzion/neon-dappkit'
 import { NetworkOption } from '../constants/config'
 import Neon, { rpc } from '@cityofzion/neon-core'
 
+/**
+ * Defines configuration options for initializing an instance of a Collection.
+ * @prop node Blockchain node URL
+ * @prop scriptHash Contract script hash
+ * @prop invoker NeonInvoker instance for invoking smart contracts
+ * @prop parser NeonParser instance for parsing responses
+ * @prop account Neon.wallet.Account for handling transactions
+ * @group Interface
+ */
 export interface ConstructorOptions {
   node?: string
   scriptHash?: string
@@ -10,12 +19,29 @@ export interface ConstructorOptions {
   account?: Neon.wallet.Account | undefined
 }
 
+/**
+ * Configuration settings for periodic polling of blockchain data.
+ * @prop period Polling interval in milliseconds
+ * @prop timeout Timeout for polling operations in milliseconds
+ * @prop node Network node selection
+ * @group Interface
+ * @interface
+ */
 export type pollingOptions = {
   period?: number
   timeout?: number
   node?: NetworkOption
 }
 
+/**
+ * Represents a structured application log from the blockchain.
+ * @prop log Raw blockchain log data
+ * @prop parsedStack Processed execution stack from the log
+ * @prop parsedNotifications Parsed event notifications from the transaction
+ * @prop parsedGASConsumption GAS consumption formatted to the 8 decimals
+ * @group Interface
+ * @interface
+ */
 export type ParsedLog = {
   log: rpc.ApplicationLogJson
   parsedStack: any
@@ -23,6 +49,11 @@ export type ParsedLog = {
   parsedGASConsumption: any
 }
 
+/**
+ * Defines options for contract invocation.
+ * @prop synchronous Determines whether the function should wait for the transaction to be completed or not
+ * @group Interface
+ */
 export interface InvocationOptions {
   synchronous: boolean
 }
